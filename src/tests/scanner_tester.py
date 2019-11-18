76fa0cf5f94a20b7a4de1d6384ccf36e7dcfddba
Richard Klem <rklem@codasip.com>
2019-11-18 13:07:31 +0100

    Upraveno vypisovani jmena zdrojaku

--- src/tests/scanner_tester.py
+++ src/tests/scanner_tester.py
@@ -19,16 +19,18 @@ def main():
     path = os.getcwd()
     input_files = []
     expected_output_files = []
+    src_ext = ('.ifj19', '.src')
+    out_ext = '.out'
 
     # r=root, d=directories, f = files
     for r, d, f in os.walk(path):
         for file in f:
-            if file.endswith('ifj19'):
+            if file.endswith(src_ext):
                 input_files.append(os.path.join(r, file))
 
     for r, d, f in os.walk(path):
         for file in f:
-            if file.endswith('.out'):
+            if file.endswith(out_ext):
                 expected_output_files.append(os.path.join(r, file))
 
     for file in input_files:
@@ -40,11 +42,12 @@ def main():
 
         actual_output = (open(actual_output_file).read()).splitlines()
         expected_output = (open(output_file).read()).splitlines()
+        test_file_name = str(file.split('/')[-1])
 
         if actual_output == expected_output:
-            print(Bcolors.OKGREEN + Bcolors.BOLD + 'PASSED' + Bcolors.ENDC + file)
+            print(Bcolors.OKGREEN + Bcolors.BOLD + 'PASSED ' + Bcolors.ENDC + test_file_name)
         else:
-            print(Bcolors.FAIL + Bcolors.BOLD + 'FAILED' + Bcolors.ENDC + file)
+            print(Bcolors.FAIL + Bcolors.BOLD + 'FAILED ' + Bcolors.ENDC + test_file_name)
             for line1, line2 in zip(expected_output, actual_output):
                 if line1.strip() and line2.strip() and line1 != line2:
                     print(Bcolors.BOLD +
