f71d430c6e40d24684e2dc0638eb6f22d59e5ba0
Richard Klem <rklem@codasip.com>
2019-11-21 09:20:43 +0100

    Pridana kostra v .h pro precedencni tabulku

--- src/parser/precedence_syntactic_analysis.h
+++ src/parser/precedence_syntactic_analysis.h
@@ -7,5 +7,10 @@ ptr_psa_stac = &psa_stack;
 
 token_t find_fisrt_terminal(ptr_psa_stac);
 
+typedef enum {
+    REDUCE, EQUAL, SHIFT, ERROR
+} precedence_rule;
+
+precedence_rule precdence_table = [8][8];
 
 #endif //IFJ_PRECEDENCE_SYNTACTIC_ANALYSIS_H
