93481557b37cee720b9c4f704e314b95df7fe79e
RichardKlem <48335467+RichardKlem@users.noreply.github.com>
2019-11-23 12:56:39 +0100

    Korektura vzhledu

--- src/parser/IFJ_precedence_table.c
+++ src/parser/IFJ_precedence_table.c
@@ -84,12 +84,12 @@ expr_token_t* find_top_terminal(tExprStack* s)
 
 precedence_table =
          {
-        //  | +-   |   /*  |   (   |    )  |   r   |   fc  |   var |    $  |
-/*|  +- |*/ {REDUCE, SHIFT,  SHIFT,  REDUCE, XXXXXX, XXXXXX, SHIFT,  REDUCE},
-/*|  /* |*/ {REDUCE, REDUCE, SHIFT,  REDUCE, XXXXXX, XXXXXX, SHIFT,  REDUCE},
-/*|  (  |*/ {SHIFT,  XXXXXX, SHIFT,  EQUAL,  XXXXXX, XXXXXX, SHIFT,  ERROR },
-/*|  )  |*/ {REDUCE, XXXXXX, ERROR,  REDUCE, XXXXXX, XXXXXX, ERROR,  REDUCE},
-/*| rel |*/ {SHIFT,  XXXXXX, XXXXXX, XXXXXX, XXXXXX, XXXXXX, SHIFT,  REDUCE},
-/*| fc  |*/ {ERROR,  ERROR,  EQUAL,  ERROR,  ERROR,  ERROR,  ERROR,  REDUCE},
-/*| var |*/ {REDUCE, REDUCE, ERROR,  REDUCE, REDUCE, ERROR,  ERROR,  REDUCE},
-/*|  $  |*/ {SHIFT,  SHIFT,  SHIFT,  ERROR,  SHIFT,  ERROR,  SHIFT,  ERROR }};
+           //  |  +-  | // / *|   (   |    )  |   r   |   fc  |   var |    $  |
+/*|   +-   |*/ {REDUCE, SHIFT,  SHIFT,  REDUCE, XXXXXX, XXXXXX, SHIFT,  REDUCE},
+/*| // / * |*/ {REDUCE, REDUCE, SHIFT,  REDUCE, XXXXXX, XXXXXX, SHIFT,  REDUCE},
+/*|   (    |*/ {SHIFT,  XXXXXX, SHIFT,  EQUAL,  XXXXXX, XXXXXX, SHIFT,  ERROR },
+/*|   )    |*/ {REDUCE, XXXXXX, ERROR,  REDUCE, XXXXXX, XXXXXX, ERROR,  REDUCE},
+/*|  rel   |*/ {SHIFT,  XXXXXX, XXXXXX, XXXXXX, XXXXXX, XXXXXX, SHIFT,  REDUCE},
+/*|  fc    |*/ {ERROR,  ERROR,  EQUAL,  ERROR,  ERROR,  ERROR,  ERROR,  REDUCE},
+/*|  var   |*/ {REDUCE, REDUCE, ERROR,  REDUCE, REDUCE, ERROR,  ERROR,  REDUCE},
+/*|   $    |*/ {SHIFT,  SHIFT,  SHIFT,  ERROR,  SHIFT,  ERROR,  SHIFT,  ERROR }};
