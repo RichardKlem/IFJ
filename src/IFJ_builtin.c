ae43b2a8f910f6b23bc9d77bc1cccd8974aae35f
Šimon Slobodník <43762416+siamones@users.noreply.github.com>
2019-11-29 00:26:35 +0100

    Update IFJ_builtin.c

--- src/IFJ_builtin.c
+++ src/IFJ_builtin.c
@@ -12,12 +12,24 @@
     a = len(b)      STRLEN a b
     a = len("abc")  STRLEN a string@abc
 
-    a = substr(retezec, zacatek, pocet) 
-    a = substr(b, c, d)    
+    a = substr(retezec, zacatek, pocet)     DEFVAR  GF@tmp
+                                            MOVE a string@
+                                            DEFVAR GF@tmp2
+                                            MOVE GF@tmp2 zacatek
+                                            DEFVAR GF@tmp3
+                                            ADD GF@tmp3 zacatek pocet
+                                            LABEL while
+                                            GT GF@tmp GF@tmp2 GF@tmp3
+                                            JUMPIFEQ end_while GF@tmp bool@false
+                                            GETCHAR tmp retezec GF@tmp2
+                                            CONCAT a GF@tmp
+                                            ADD GF@tmp2 GF@tmp2 int@1
+                                            JUMP wjile
+                                            LABEL end_while
     
-    a = ord(řetězec. pozice)    
+    a = ord(řetězec. pozice)    STRI2INT a řetězec pozice
 
-    a = chr(ASCII_kod)
+    a = chr(ASCII_kod)      INT2CHAR a ASCII_kod
 
 
 */
