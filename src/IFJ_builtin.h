b152e933e09fb4811499cfa90c94fae337c0df99
Martin Haderka <xhader00@stud.fit.vutbr.cz>
2019-11-29 19:38:15 +0100

    Update IFJ_builtin.h

--- src/IFJ_builtin.h
+++ src/IFJ_builtin.h
@@ -3,4 +3,14 @@
 #ifndef IFJ_BUILTIN_H
 #define IFJ_BUILTIN_H
 
+void generate_builtin();
+void call_inputi();
+void call_inputf();
+void call_inputs();
+void call_write(char* arg);
+void call_len(char* arg);
+void call_substr(char* arg1, char* arg2, char* arg3);
+void call_ord(char* arg1, char* arg2);
+void call_chr(char* arg);
+
 #endif //IFJ_BUILTIN_H
