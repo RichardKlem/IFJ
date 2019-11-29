dab0069f56390d25b6c6a830982643c8ae0c2eb2
Šimon Slobodník <43762416+siamones@users.noreply.github.com>
2019-11-29 11:58:07 +0100

    Update IFJ_builtin.c

--- src/IFJ_builtin.c
+++ src/IFJ_builtin.c
@@ -30,6 +30,128 @@
     a = ord(řetězec. pozice)    STRI2INT a řetězec pozice
 
     a = chr(ASCII_kod)      INT2CHAR a ASCII_kod
-
-
 */
+void generate_builtin()
+{
+    //Definovani vestavene funkce inputi
+    printf("#def inputi()\n");
+    printf("JUMP end_$inputi\n");
+    printf("LABEL $inputi\n");
+    printf("PUSHFRAME\n");
+    printf("DEFVAR LF@\cret\n", '%');
+    printf("MOVE LF@\cret nil@nil\n", '%');
+    printf("READ LF@\cret int\n", '%');
+    printf("POPFRAME\n");
+    printf("RETURN\n");
+    printf("LABEL end_$inputi\n");
+    printf("\n");
+
+    //Definovani vestavene funkce inputf
+    printf("#def inputf()\n");
+    printf("JUMP end_$inputf\n");
+    printf("LABEL $inputf\n");
+    printf("PUSHFRAME\n");
+    printf("DEFVAR LF@\cret\n", '%');
+    printf("MOVE LF@\cret nil@nil\n", '%');
+    printf("READ LF@\cret float\n", '%');
+    printf("POPFRAME\n");
+    printf("RETURN\n");
+    printf("LABEL end_$inputf\n");
+    printf("\n");
+
+    //Definovani vestavene funkce inputs
+    printf("#def inputs()\n");
+    printf("JUMP end_$inputs\n");
+    printf("LABEL $inputs\n");
+    printf("PUSHFRAME\n");
+    printf("DEFVAR LF@\cret\n", '%');
+    printf("MOVE LF@\cret nil@nil\n", '%');
+    printf("READ LF@\cret string\n", '%');
+    printf("POPFRAME\n");
+    printf("RETURN\n");
+    printf("LABEL end_$inputs\n");
+    printf("\n");
+
+    //Definovani vestavene funkce write
+    printf("#def print()\n");
+    printf("JUMP end_$print\n");
+    printf("LABEL $print\n");
+    printf("PUSHFRAME\n");
+    printf("DEFVAR LF@param\n",);
+    printf("MOVE LF@param LF@\c1\n", '%');
+    printf("WRITE LF@param\n", '%');
+    printf("POPFRAME\n");
+    printf("RETURN\n");
+    printf("LABEL end_$print\n");
+
+    //Definovani vestavene funkce len
+    printf("#def len()\n");
+    printf("JUMP end_$len\n");
+    printf("LABEL $len\n");
+    printf("PUSHFRAME\n");
+    printf("DEFVAR LF@\cret\n", '%');
+    printf("MOVE LF@\cret nil@nil\n", '%');
+    printf("DEFVAR LF@param\n");
+    printf("MOVE LF@param LF@\c1\n", '%', '%');
+    printf("STRLEN LF@\cret LF@param\n", '%');
+    printf("POPFRAME\n");
+    printf("RETURN\n");
+    printf("LABEL end_$len\n");
+
+    //Definovani vestavene funkce substr
+    printf("#def substr()\n");
+    printf("JUMP end_$substr\n");
+    printf("LABEL $substr\n");
+    printf("PUSHFRAME\n");
+    printf("DEFVAR LF@\cret\n", '%');
+    printf("MOVE LF@\cret string@\n", '%');
+    printf("DEFVAR LF@param1\n");
+    printf("MOVE LF@param1 LF@\c1\n", '%');
+    printf("DEFVAR LF@param2\n", '%');
+    printf("MOVE LF@param2 LF@\c2\n", '%');
+    printf("DEFVAR LF@param3\n", '%');
+    printf("MOVE LF@param3 LF@\c3\n", '%');
+    printf("ADD LF@param3 LF@param3 LF@param2\n");
+    printf("DEFVAR L$tmp\n");
+    printf("LABEL $while_substr\n");
+    printf("GT LF@tmp LF@param2 LF@param3\n");
+    printf("JUMPIFEQ $end_while_substr LF@tmp bool@false\n");
+    printf("GETCHAR LF@tmp LF@param1 LF@param2\n");
+    printf("CONCAT LF@\cret LF@tmp\n", '%');
+    printf("ADD LF@param2 LF@param2 int@1\n");
+    printf("JUMP $while_substr\n");
+    printf("LABEL $end_while_substr\n");
+    printf("POPFRAME\n");
+    printf("RETURN\n");
+    printf("LABEL end_$substr\n");
+
+    //Definice vestavene funkce ord
+    printf("#def ord()\n");
+    printf("JUMP end_$ord\n");
+    printf("LABEL $ord\n");
+    printf("PUSHFRAME\n");
+    printf("DEFVAR LF@\cret\n", '%');
+    printf("MOVE LF@\cret nil@nil\n", '%');
+    printf("DEFVAR LF@param1\n");
+    printf("MOVE LF@param1 LF@\c1\n", '%');
+    printf("DEFVAR LF@param2\n");
+    printf("MOVE LF@param2 LF@\c2\n", '%');
+    printf("STRI2INT LF@\cret LF@param1 LF@param2\n", '%');
+    printf("POPFRAME\n");
+    printf("RETURN\n");
+    printf("LABEL end_$ord\n");
+
+    //Definice vestavene funkce chr
+    printf("#def chr()\n");
+    printf("JUMP end_$chr\n");
+    printf("LABEL $chr\n");
+    printf("PUSHFRAME\n");
+    printf("DEFVAR LF@\cret\n", '%');
+    printf("MOVE LF@\cret nil@nil\n", '%');
+    printf("DEFVAR LF@param1\n");
+    printf("MOVE LF@param1 LF@\c1\n", '%');
+    printf("INT2CHAR LF@\cret LF@param1\n", '%');
+    printf("POPFRAME\n");
+    printf("RETURN\n");
+    printf("LABEL end_$chr\n");
+}
